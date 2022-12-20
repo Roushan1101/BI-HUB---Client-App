64a7d2edaf79b688c01efc6da3f9bffc456937f1
Roushan Kumar <v-rouskumar@microsoft.com>
2022-12-20 11:14:51 +0530

    Updates

---
+++ precache-manifest.17087f3f9a9110bd0bb5189754a37a90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b3dcdde808763fa1017620a8d55d25",
+    "url": "/BI-HUB---Client-App/index.html"
+  },
+  {
+    "revision": "26c0f504c248434669bc",
+    "url": "/BI-HUB---Client-App/static/css/2.d7b63610.chunk.css"
+  },
+  {
+    "revision": "4f607bc17cc905e0d1cc",
+    "url": "/BI-HUB---Client-App/static/css/main.5c119121.chunk.css"
+  },
+  {
+    "revision": "26c0f504c248434669bc",
+    "url": "/BI-HUB---Client-App/static/js/2.a621950e.chunk.js"
+  },
+  {
+    "revision": "0665cc432fdf5ae3f3433075c30e3f5e",
+    "url": "/BI-HUB---Client-App/static/js/2.a621950e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f607bc17cc905e0d1cc",
+    "url": "/BI-HUB---Client-App/static/js/main.de49fe2c.chunk.js"
+  },
+  {
+    "revision": "7a42263da33594c42094",
+    "url": "/BI-HUB---Client-App/static/js/runtime-main.bb7d2001.js"
+  },
+  {
+    "revision": "876d6c05e9575e6bebaebd04bafab868",
+    "url": "/BI-HUB---Client-App/static/media/search_icon.876d6c05.png"
+  }
+]);
